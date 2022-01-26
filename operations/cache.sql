5b12a23de5411c5e83ec2a30f1d094fb1cb51569
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2022-01-26 13:24:33 +0530

    Create cache.sql

---
+++ operations/cache.sql
@@ -0,0 +1,21 @@
+-- these are the cache tables in pega 
+truncate table pr_sys_appcache_entry;
+truncate table pr_sys_appcache_dep;
+truncate table pr_sys_appcache_shortcut;
+truncate table pr_assembledclasses;
+truncate table pr_sys_rule_impl;
+truncate table pr4_rule_sysgen;
+
+truncate table pr_sys_appcache_entry;
+truncate table pr_sys_appcache_dep;
+truncate table pr_sys_appcache_shortcut;
+truncate table pr_assembledclasses;
+truncate table pr_sys_rule_impl;
+truncate table pr4_rule_sysgen;
+
+PR4_RULE_SYSGEN - PR4_RULE_SYSGEN_IA0004555 backup table
+PR_ASSEMBLEDCLASSES - PR_ASSEMBLEDCLASSES_IA0004555 backup table
+PR_SYS_APPCACHE_DEP - PR_SYS_APPCACHE_DEP_IA0004555 backup table
+PR_SYS_APPCACHE_ENTRY - PR_SYS_APEN_IA0004555 backup table
+PR_SYS_APPCACHE_SHORTCUT - PR_SYS_APSH_IA0004555 backup table
+PR_SYS_RULE_IMPL - PR_SYS_RULE_IMPL_IA0004555 backup table
