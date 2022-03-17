3c77b5c7927a8b992a2514724ada363b4a5e74ee
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2022-03-17 15:53:56 +0530

    final corrections

--- lsa/user interface/clearactionsequence.js
+++ lsa/user interface/clearactionsequence.js
@@ -1,6 +1,8 @@
-/*If conditionally to remove action sequeunce configured in any HTML in Pega
+/*
+If conditionally to remove action sequeunce configured in any HTML element in Pega ( links, buttons and etc..
 use example piece of code like 
-clrActions - will have either true or value*/
+clrActions - will have either true or false value
+*/
 
 function ClearActionQueue(clrActions){
   if (clrActions === "true"){
@@ -11,4 +13,4 @@ function ClearActionQueue(clrActions){
   
   /* use the above function in runScript action and pass the property as input parameter to that )
   
-  /* Note - Please keep the above function in any of the JS Files acceisble to HTML element - likey in Portal level harness otherwise in Perform harness ) */
+  /* Note - Please keep the above function in any of the JS Files accesible to HTML element - likely in Portal level harness otherwise in Perform harness ) */
