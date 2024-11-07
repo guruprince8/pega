9af22e9d1153d26e02410f17a8cff93e66f8fef0
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-07 10:25:06 -0600

    Update kubesetup.sh
    added initial logic

--- 242/kubesetup.sh
+++ 242/kubesetup.sh
@@ -3,14 +3,6 @@ kubectl config use-context docker-desktop
 helm version ; helm repo list;
 kubectl create namespace pega ; kubectl create namespace pegaaddons ; kubectl create namespace pegabackingservices
 
-# kafka set up
-helm uninstall kafka -n pega
-helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka -n pega
-helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka --namespace pega
-kubectl exec -it kafka-controller-0 -n pega -- /bin/sh
-(kubectl get secret kafka-user-passwords --namespace pega -o jsonpath='{.data.client-passwords}' | base64 -d | cut -d , -f 1)
-
-
 helm install backingservices pega/backingservices --namespace pegabackingservices --values backingservices.yaml
 helm install addons pega/addons --namespace pegaaddons --values addons.yaml
 helm install pega pega/pega --namespace pega --values pega.yaml
