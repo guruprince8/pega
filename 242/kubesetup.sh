638b808382df4d895d3c95bb42c0c3523b630d63
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-07 11:18:53 -0600

    Update kubesetup.sh
    added initial logic

--- 242/kubesetup.sh
+++ 242/kubesetup.sh
@@ -1,4 +1,3 @@
-sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0
 kubectl config use-context docker-desktop
 helm version ; helm repo list;
 kubectl create namespace pega ; kubectl create namespace pegaaddons ; kubectl create namespace pegabackingservices
@@ -19,32 +18,11 @@ kubectl exec -it pega-search-0 -n pega -- /bin/sh
 kubectl exec -it  pega-minikube-0 -n pega -- /bin/sh
 tail -f /usr/local/tomcat/logs/pega-minikube-0/PegaRULES.log
 
-
-
 [sudo] password for guru:
 WARNING! Using --password via the CLI is insecure. Use --password-stdin.
 WARNING! Your password will be stored unencrypted in /root/.docker/config.json.
 Configure a credential helper to remove this warning. See
 https://docs.docker.com/engine/reference/commandline/login/#credential-stores
-
-Login Succeeded
-
-~/kafka/bin/kafka-topics.sh --create --bootstrap-server 192.168.1.218:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic
-
-~/kafka/bin/kafka-topics.sh --create --bootstrap-server 192.168.1.218:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic2
-
-~/kafka/bin/kafka-topics.sh --bootstrap-server 192.168.1.218:9092 --list
-
-~/kafka/bin/kafka-topics.sh --describe --bootstrap-server 192.168.1.218:9092 --topic TutorialTopic
-
-~/kafka/bin/kafka-topics.sh --version
-
-curl https://downloads.apache.org/kafka/3.6.2/kafka_2.13-3.6.2.tgz -o kafka.tgz
-
-tar -xvzf ../kafka.tgz --strip 1
-
-nc -zv 192.168.1.218 9092
-
 
 curl http://pega-search
 {
