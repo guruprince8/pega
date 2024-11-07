3ae3853b4612b65f9738e083e9a8019af4125a2d
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-07 12:42:45 -0600

    Create all_pod.sh

---
+++ 242/kube/pod/all_pod.sh
@@ -0,0 +1,8 @@
+/* kubectl get pods -n pega */
+NAME                 READY   STATUS    RESTARTS   AGE
+kafka-controller-0   1/1     Running   0          4h27m
+kafka-controller-1   1/1     Running   0          4h27m
+kafka-controller-2   1/1     Running   0          4h27m
+pega-minikube-0      1/1     Running   0          4h3m
+pega-search-0        1/1     Running   0          4h3m
+*/
