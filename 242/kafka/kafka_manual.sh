e5ac0a261d9b37e9dda03f4e2db13fcca434429a
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-07 11:18:33 -0600

    Update kafka_manual.sh
    ~/kafka/bin/kafka-topics.sh --create --bootstrap-server 192.168.1.218:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic
    
    ~/kafka/bin/kafka-topics.sh --create --bootstrap-server 192.168.1.218:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic2
    
    ~/kafka/bin/kafka-topics.sh --bootstrap-server 192.168.1.218:9092 --list
    
    ~/kafka/bin/kafka-topics.sh --describe --bootstrap-server 192.168.1.218:9092 --topic TutorialTopic
    
    ~/kafka/bin/kafka-topics.sh --version
    
    curl https://downloads.apache.org/kafka/3.6.2/kafka_2.13-3.6.2.tgz -o kafka.tgz
    
    tar -xvzf ../kafka.tgz --strip 1
    
    nc -zv 192.168.1.218 9092

--- 242/kafka/kafka_manual.sh
+++ 242/kafka/kafka_manual.sh
@@ -32,3 +32,20 @@ password="admin"
 user_admin="admin"
 user_alice="alice";
 };
+
+
+~/kafka/bin/kafka-topics.sh --create --bootstrap-server 192.168.1.218:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic
+
+~/kafka/bin/kafka-topics.sh --create --bootstrap-server 192.168.1.218:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic2
+
+~/kafka/bin/kafka-topics.sh --bootstrap-server 192.168.1.218:9092 --list
+
+~/kafka/bin/kafka-topics.sh --describe --bootstrap-server 192.168.1.218:9092 --topic TutorialTopic
+
+~/kafka/bin/kafka-topics.sh --version
+
+curl https://downloads.apache.org/kafka/3.6.2/kafka_2.13-3.6.2.tgz -o kafka.tgz
+
+tar -xvzf ../kafka.tgz --strip 1
+
+nc -zv 192.168.1.218 9092
