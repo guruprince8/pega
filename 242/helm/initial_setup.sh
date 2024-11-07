60ea7aa690f77b06eef4685d3bb7ba136155b976
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-07 12:53:02 -0600

    Update initial_setup.sh
    added ini logic

--- 242/helm/initial_setup.sh
+++ 242/helm/initial_setup.sh
@@ -5,8 +5,12 @@ helm inspect values pega/addons > addons.yaml
 helm inspect values pega/backingservices > backingservices.yaml
 helm repo list
 
+helm uninstall pega --namespace pega
+helm install pega pega/pega --namespace pega --values values-local.yaml
 helm install backingservices pega/backingservices --namespace pegabackingservices --values backingservices.yaml
 helm install addons pega/addons --namespace pegaaddons --values addons.yaml
 helm install pega pega/pega --namespace pega --values pega.yaml
+
+
 
 
