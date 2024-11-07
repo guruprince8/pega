344a6872bae53b4c3389dc9529ae3e6eeb56a736
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-07 12:45:38 -0600

    Update all_service.sh
    added initial logic

--- 242/kube/service/all_service.sh
+++ 242/kube/service/all_service.sh
@@ -1,12 +1,3 @@
-/* kubectl get pods -n pega */
-NAME                 READY   STATUS    RESTARTS   AGE
-kafka-controller-0   1/1     Running   0          4h27m
-kafka-controller-1   1/1     Running   0          4h27m
-kafka-controller-2   1/1     Running   0          4h27m
-pega-minikube-0      1/1     Running   0          4h3m
-pega-search-0        1/1     Running   0          4h3m
-*/
-
 /* kubectl get service -n pega *
 NAME                        TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)                      AGE
 kafka                       ClusterIP   10.98.27.146     <none>        9092/TCP                     4h28m
