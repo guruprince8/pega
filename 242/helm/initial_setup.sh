adeb33fac40f6485377160145435905878d92307
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-12 11:05:00 -0600

    Update initial_setup.sh
    applied logic for specific environment

--- 242/helm/initial_setup.sh
+++ 242/helm/initial_setup.sh
@@ -7,9 +7,23 @@ helm repo list
 
 helm uninstall pega --namespace pega
 helm install pega pega/pega --namespace pega --values values-local.yaml
+
 helm install backingservices pega/backingservices --namespace pegabackingservices --values backingservices.yaml
 helm install addons pega/addons --namespace pegaaddons --values addons.yaml
 helm install pega pega/pega --namespace pega --values pega.yaml
+
+/* SIT Environment */
+helm install pega-sit pega/pega --namespace pega-prod --values values-local-sit.yaml
+
+/* E2E Environment */
+helm install pega-e2e pega/pega --namespace pega-prod --values values-local-e2e.yaml
+
+/* CERT Environment */
+helm install pega-cert pega/pega --namespace pega-prod --values values-local-cert.yaml
+
+/* PROD Environment */
+helm install pega-prod pega/pega --namespace pega-prod --values values-local-prod.yaml
+
 
 
 
