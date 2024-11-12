26dc833a65708237917270d1720ba04c21fda82f
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-12 11:35:56 -0600

    Update initial_setup.sh
    added initial logic

--- 242/helm/initial_setup.sh
+++ 242/helm/initial_setup.sh
@@ -11,13 +11,15 @@ helm install pega pega/pega --namespace pega --values values-local.yaml
 helm install backingservices pega/backingservices --namespace pegabackingservices --values backingservices.yaml
 helm install addons pega/addons --namespace pegaaddons --values addons.yaml
 helm install pega pega/pega --namespace pega --values pega.yaml
+tail -f /usr/local/tomcat/logs/pega-minikube-0/PegaRULES.log
+
 
 /* SIT Environment */
 helm uninstall pega-sit --namespace pega-sit
 helm install pega-sit pega/pega --namespace pega-prod --values values-local-sit.yaml
 kubectl exec -it pega-search-0 -n pega-sit -- /bin/sh
 kubectl exec -it pega-minikube-0 -n pega-sit -- /bin/sh
-
+tail -f /usr/local/tomcat/logs/pega-minikube-0/PegaRULES.log
 
 
 /* E2E Environment */
@@ -26,7 +28,7 @@ helm install pega-e2e pega/pega --namespace pega-prod --values values-local-e2e.
 kubectl exec -it pega-search-0 -n pega-e2e -- /bin/sh
 kubectl exec -it pega-minikube-0 -n pega-e2e -- /bin/sh
 helm upgrade pega-prod pega/pega --namespace pega-prod --values values-local-prod.yaml
-
+tail -f /usr/local/tomcat/logs/pega-minikube-0/PegaRULES.log
 
 
 /* CERT Environment */
@@ -34,6 +36,7 @@ helm uninstall pega-cert --namespace pega-cert
 helm install pega-cert pega/pega --namespace pega-prod --values values-local-cert.yaml
 kubectl exec -it pega-search-0 -n pega-cert -- /bin/sh
 kubectl exec -it pega-minikube-0 -n pega-cert -- /bin/sh
+tail -f /usr/local/tomcat/logs/pega-minikube-0/PegaRULES.log
 
 
 
@@ -42,6 +45,7 @@ helm uninstall pega-prod --namespace pega-prod
 helm install pega-prod pega/pega --namespace pega-prod --values values-local-prod.yaml
 kubectl exec -it pega-search-0 -n pega-prod -- /bin/sh
 kubectl exec -it pega-minikube-0 -n pega-prod -- /bin/sh
+tail -f /usr/local/tomcat/logs/pega-minikube-0/PegaRULES.log
 
 
 
