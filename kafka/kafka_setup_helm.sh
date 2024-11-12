ddb1911c726a7c97c3729a7632a038c054800968
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-11 21:45:45 -0600

    Update kafka_setup_helm.sh

--- kafka/kafka_setup_helm.sh
+++ kafka/kafka_setup_helm.sh
@@ -3,3 +3,5 @@ helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka -n pega
 helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka --namespace pega
 kubectl exec -it kafka-controller-0 -n pega -- /bin/sh
 (kubectl get secret kafka-user-passwords --namespace pega -o jsonpath='{.data.client-passwords}' | base64 -d | cut -d , -f 1)
+kubectl exec -it kafka-controller-0 -n pega -- /bin/sh
+cd /bitnami/kafka/data
