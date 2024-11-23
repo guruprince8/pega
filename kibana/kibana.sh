a8ba5a303e3751798e5c05158a4a28eda65f1f54
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-23 10:23:17 -0600

    Create kibana.sh
    added initial logic

---
+++ kibana/kibana.sh
@@ -0,0 +1,6 @@
+# https://www.digitalocean.com/community/tutorials/how-to-install-elasticsearch-logstash-and-kibana-elastic-stack-on-ubuntu-22-04
+sudo apt install kibana
+sudo systemctl enable kibana
+sudo systemctl start kibana
+
+# https://www.elastic.co/guide/en/kibana/7.17/kuery-query.html
