549d54808d56d9c9bf8d62db7480f5c09324544d
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-23 08:31:55 -0600

    Update elasticsearch.sh

--- elastic/elasticsearch.sh
+++ elastic/elasticsearch.sh
@@ -1,1 +1,29 @@
+# https://www.elastic.co/docs
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/docs.html
 
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-elasticsearch-on-ubuntu-22-04
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic.gpg
+echo "deb [signed-by=/usr/share/keyrings/elastic.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt update & sudo apt install elasticsearch
+sudo nano /etc/elasticsearch/elasticsearch.yml
+sudo systemctl start elasticsearch
+sudo systemctl enable elasticsearch
+sudo ufw allow from 198.51.100.0 to any port 9200
+sudo ufw enable
+sudo ufw status
+curl -X GET 'http://localhost:9200'
+curl -X GET 'http://localhost:9200/_nodes?pretty'
+
+# post data
+curl -XPOST -H "Content-Type: application/json" 'http://localhost:9200/tutorial/helloworld/1' -d '{ "message": "Hello World!" }'
+
+# get data
+curl -X GET -H "Content-Type: application/json" 'http://localhost:9200/tutorial/helloworld/1'
+
+# modfiy existing record
+curl -X PUT -H "Content-Type: application/json"  'localhost:9200/tutorial/helloworld/1?pretty' -d '
+{
+  "message": "Hello, People!"
+}'
+
+curl -X GET -H "Content-Type: application/json" 'http://localhost:9200/tutorial/helloworld/1?pretty'
