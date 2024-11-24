858b88944b2dc5dabf3e1ff7335c0d9ce053681e
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-11-23 18:34:25 -0600

    Update Dockerbuild.sh

--- ecommerceapi/Dockerbuild.sh
+++ ecommerceapi/Dockerbuild.sh
@@ -1,4 +1,4 @@
 # https://hub.docker.com/repository/docker/guruprince8/ecommerceapi/general
-sudo docker build -t guruprince8:/ecommerceapi:1.0 .
+sudo docker build -t guruprince8/ecommerceapi:1.0 .
 sudo docker login -u guruprince8
 sudo docker push guruprince8/ecommerceapi:1.0
