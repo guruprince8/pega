6d45937d2b2728a1cc9880c375c546a2b213d9b0
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-12-30 20:46:25 -0600

    Create create_cert.sh

---
+++ 242/certs/create_cert.sh
@@ -0,0 +1,1 @@
+keytool -genkey -keyalg RSA -alias tomcat -keystore tomcat.jks -storepass tomcat -validity 360 -keysize 2048 -ext SAN=dns:allendc630,ip:127.0.0.1,ip:192.168.1.223,ip:192.168.1.221,dns:Mac.attlocal.net
