6d7fe679329e1c6705314b50ed5de765a92a3e14
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-12-30 20:49:21 -0600

    Update create_cert.sh

--- 242/certs/create_cert.sh
+++ 242/certs/create_cert.sh
@@ -1,2 +1,3 @@
 keytool -genkey -keyalg RSA -alias tomcat -keystore tomcat.jks -storepass tomcat -validity 360 -keysize 2048 -ext SAN=dns:allendc630,ip:127.0.0.1,ip:192.168.1.223,ip:192.168.1.221,dns:Mac.attlocal.net
 keytool --list -v -keystore tomcat.jks
+cat tomcat.jks | base64 -w 0
